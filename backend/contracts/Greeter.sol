ec36c9f013048bc3ee7eb5167ee10f3b0a964ecc
Nicolas Diz <diz.nicolasamor@gmail.com>
2021-12-06 11:46:08 -0300

    MASTER: update Greeter.sol

--- backend/contracts/Greeter.sol
+++ backend/contracts/Greeter.sol
@@ -4,19 +4,22 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Greeter {
-    string private greeting;
+    string private name;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    event nameUpdated(string eventOutput);
+
+    constructor(string memory _name) {
+        console.log("Deploying a Greeter with name:", _name);
+        name = _name;
     }
 
     function greet() public view returns (string memory) {
-        return greeting;
+        return string(abi.encodePacked("Hello", ' ', name));
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function setName(string memory _name) public {
+        console.log("Changing name from '%s' to '%s'", name, _name);
+        name = _name;
+        emit nameUpdated(_name);
     }
 }
