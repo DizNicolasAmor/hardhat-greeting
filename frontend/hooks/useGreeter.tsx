de54706c44fe40adfeed147dec295c71eaf8d0de
Nicolas Diz <diz.nicolasamor@gmail.com>
2021-12-07 12:36:14 -0300

    MASTER: refactor frontend

---
+++ frontend/hooks/useGreeter.tsx
@@ -0,0 +1,51 @@
+import { ethers, providers } from 'ethers';
+import { useEffect, useState } from 'react';
+import Greeter from '../artifacts/contracts/Greeter.sol/Greeter.json';
+import { CONTRACT_ADDRESSES } from '../utils/constants';
+
+type Web3Props = {
+  web3: providers.Web3Provider | undefined;
+};
+
+const useGreeter = ({ web3 }: Web3Props) => {
+  const [address, setAddress] = useState<string>(CONTRACT_ADDRESSES.LOCAL);
+
+  useEffect(() => {
+    web3?.detectNetwork().then((network) => {
+      const differentAddress: string = CONTRACT_ADDRESSES[network.name];
+      if (differentAddress) {
+        setAddress(differentAddress);
+      } else {
+        console.warn(`Contract address not found in network: ${network.name}`);
+      }
+    });
+  }, [web3]);
+
+  const fetchGreet = async () => {
+    if (!web3) return;
+
+    const contract = new ethers.Contract(address, Greeter.abi, web3);
+
+    try {
+      const greeting = await contract.greet();
+      return greeting;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  const fetchSetName = async (name: string) => {
+    if (!web3) return;
+    if (!name) throw new Error('Invalid param');
+
+    const signer = web3.getSigner();
+    const contract = new ethers.Contract(address, Greeter.abi, signer);
+    const transaction = await contract.setGreeting(name);
+    await transaction.wait();
+  };
+
+  return [fetchGreet, fetchSetName] as const;
+};
+
+export default useGreeter;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { ethers } from 'ethers';
-import Greeter from '../artifacts/contracts/Greeter.sol/Greeter.json';
+import React, { useEffect, useRef, useState } from 'react';
+import { providers } from 'ethers';
+import useNetwork from '../hooks/useNetwork';
+import useGreeter from '../hooks/useGreeter';
 
 declare global {
   interface Window {
@@ -10,42 +11,41 @@ declare global {
 }
 
 const Home = () => {
+  const [network, setNetwork] = useState<providers.Network>();
+  const [account, setAccount] = useState<string>();
   const [errorMessage, setErrorMessage] = useState('');
-  const [defaultAccount, setDefaultAccount] = useState('');
   const [userBalance, setUserBalance] = useState('');
-  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const [greeting, setGreeting] = useState('');
 
-  const [provider, setProvider] = useState(null);
-  const [signer, setSigner] = useState('');
-  const [contract, setContract] = useState(null);
+  const [{ web3 }, handleNetwork] = useNetwork();
+  const [fetchGreet, fetchSetName] = useGreeter({ web3 });
 
-  const connectWalletHandler = () => {
-    const hasMetaMask: boolean = window.ethereum && window.ethereum.isMetaMask;
-    if (!hasMetaMask) {
-      setErrorMessage('Need to install MetaMask');
-      return;
+  useEffect(() => {
+    if (typeof web3 === 'undefined') {
+      resetFields();
+    } else {
+      const setNetworkAccount = async () => {
+        web3.detectNetwork().then(setNetwork).catch(setErrorMessage);
+        web3
+          .listAccounts()
+          .then((accounts) => {
+            setAccount(accounts[0]);
+          })
+          .catch(setErrorMessage);
+      };
+      setNetworkAccount();
     }
-    window.ethereum
-      .request({ method: 'eth_requestAccounts' })
-      .then((result: string[]) => {
-        accountChangedHandler(result[0]);
-        setConnButtonText('Wallet Connected');
-        getAccountBalance(result[0]);
-      })
-      .catch((error: unknown) => {
-        console.error(error);
-        setErrorMessage('Error connecting MetaMask');
-      });
+  }, [web3]);
+
+  const resetFields = () => {
+    setNetwork(undefined);
+    setAccount('');
+    setGreeting('');
   };
 
-  const accountChangedHandler = (newAccount: string) => {
-    setDefaultAccount(newAccount);
-    getAccountBalance(newAccount);
-    updateEthers();
-  };
-
+  /*
   const getAccountBalance = (account: string) => {
     window.ethereum
       .request({ method: 'eth_getBalance', params: [account, 'latest'] })
@@ -58,38 +58,32 @@ const Home = () => {
         setErrorMessage('Error getting balance');
       });
   };
+  */
 
-  const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-
-  const updateEthers = () => {
-    const tempProvider: any = new ethers.providers.Web3Provider(
-      window.ethereum
-    );
-    setProvider(tempProvider);
-
-    const tempSigner: any = tempProvider.getSigner();
-    setSigner(tempSigner);
-
-    const tempContract: any = new ethers.Contract(
-      contractAddress,
-      Greeter.abi,
-      tempSigner
-    );
-    setContract(tempContract);
-
-    console.log({ tempProvider, tempContract, tempSigner });
+  const handleConnect = () => {
+    handleNetwork().catch(setErrorMessage);
   };
 
-  const getGreeting = async () => {
-    const internalGreeting = await contract.greet();
-    setGreeting(internalGreeting);
+  const getGreeting = () => {
+    fetchGreet().then(setGreeting).catch(setErrorMessage);
+  };
+
+  const handleSetName = () => {
+    if (inputRef.current) {
+      fetchSetName(inputRef.current.value)
+        .then(() => fetchGreet())
+        .catch(setErrorMessage);
+    }
   };
 
   return (
     <div>
       <h1>Greeter dApp</h1>
-      <button onClick={connectWalletHandler}>{connButtonText}</button>
-      <div>Address: {defaultAccount}</div>
+      <button onClick={handleConnect}>{web3 ? 'Disconnect' : 'Connect'}</button>
+      <div>
+        Network: {network?.chainId} {network?.name}
+      </div>
+      <div>Address: {account}</div>
       <div>Balance: {userBalance}</div>
       <div aria-live="assertive" aria-atomic="true">
         {errorMessage}
@@ -97,6 +91,8 @@ const Home = () => {
 
       <button onClick={getGreeting}>GET GREETING FROM SMART CONTRACT</button>
       <div>{greeting}</div>
+      <button onClick={handleSetName}>Set Greeting</button>
+      <input ref={inputRef} placeholder="Set greeting" />
     </div>
   );
 };
